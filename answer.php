a3b09ec08edb220c3c79a5ace9e903f9d690ad45
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-04 23:41:38 +0000

    index+answer.php

---
+++ answer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!-- ICS2O-Unit3-03-PHP -->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Volume of a Sphere, with PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Peter Zerbinos" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.orange-pink.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="./apple-touch-icon.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="./favicon-32x32.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Volume of a Sphere, with PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Volume of a Sphere, with PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/volume-of-sphere.png" alt="sphere" />
+      </div>
+      <?php
+      $radiusOfSphere = $_GET["radius-of-sphere"];
+
+      // process
+      $volumeOfSphere = (4 / 3) * (M_PI) * ($radiusOfSphere ** 3);
+
+
+      // output
+      echo "For a sphere with a radius of " . $radiusOfSphere . "," . " the volume is: " . round($volumeOfSphere * 1, 2) . " mm³.";
+      ?>
+      <br />
+      <div class="page-content">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
